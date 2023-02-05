2f860548bf8c2fb7ed20fd3ccfd4f9140b8d31c6
keviner99 <90742482+keviner99@users.noreply.github.com>
2023-02-04 21:30:31 -0500

    update index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
-router.use('/userinfo', require('./userinfo'))
+router.use('/userInfo', require('./userinfo'))
 router.use('/recipeinfo', require('./recipeinfo'))
 router.use('/comment', require('./comment'))
 router.use('/communities', require('./communities'))
